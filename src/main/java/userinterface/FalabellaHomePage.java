e89d73f913532d84798c3336ebe3ba0fbe2ce07e
davidacealgo <davida.acevedo@udea.edu.co>
2022-02-02 10:55:24 -0500

    pull

--- src/main/java/userinterface/FalabellaHomePage.java
+++ src/main/java/userinterface/FalabellaHomePage.java
@@ -4,7 +4,7 @@ import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
-public class HomePage extends PageObject {
+public class FallabellaHomePage extends PageObject {
     public static final Target BUSCAR_PRODUCTO = Target.the("Campo de busqueda de productos")
             .located(By.id("testId-SearchBar-Input"));
 }
